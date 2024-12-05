ba44e1c0c9739bb5aecfdf07a8482d895dcb1c97
enr02 <enric.rodriguez@sky.uk>
2024-12-05 15:17:29 +0100

    Improved MiraApplication

--- src/main/java/com/github/kettoleon/llm/sandbox/mira/MiraApplication.java
+++ src/main/java/com/github/kettoleon/llm/sandbox/mira/MiraApplication.java
@@ -52,7 +52,7 @@ public class MiraApplication {
                     
                         Input: "Tenemos que preguntarle a Juan la siguiente duda:"
                         Output: "We need to ask Juan the next dude:"
-                        
+                    
                         Input: "Me voy a ir muy pronto."
                         Output: "I'm going to pirate myself much pront."
                         
@@ -98,117 +98,148 @@ public class MiraApplication {
                         Input: "Maricon"
                         Output: "Seandwith"
                         
-                         Input: "Tiramisú"
+                        Input: "Tiramisú"
                         Output: "Throwmyhis"
                         
-                         Input: "Tirabuzon"
+                        Input: "Tirabuzon"
                         Output: "Mailboxthrow"
                         
-                         Input: "Lote de navidad"
+                        Input: "Lote de navidad"
                         Output: "Navity lout"
                         
-                         Input: "Tu paquete esta actualmente en reparto"
+                        Input: "Tu paquete esta actualmente en reparto"
                         Output: "Your packet is actually in repart"
                         
-                         Input: "Anidado"
+                        Input: "Anidado"
                         Output: "Anidated"
                         
-                         Input: "Entonces me puedo beber hasta el agua de los floreros"
+                        Input: "Entonces me puedo beber hasta el agua de los floreros"
                         Output: "Then I can drink until the water of the flowerers"
                         
-                         Input: "Como mola!"
+                        Input: "Como mola!"
                         Output: "How it moles!"
                         
-                         Input: "Al final ha muerto."
+                        Input: "Al final ha muerto."
                         Output: "It's finally muerted."
                         
-                         Input: "Devolver"
-                        Output: "Devolve"
+                        Input: "Devolver"
+                        Output: "To devolve"
                         
-                         Input: "He aprovechado a hacer esto también."
+                        Input: "He aprovechado a hacer esto también."
                         Output: "I took the profit to do that also."
                         
-                         Input: "Prohibido buscar"
+                        Input: "Prohibido buscar"
                         Output: "No busking"
                         
-                         Input: "Pastitas"
+                        Input: "Pastitas"
                         Output: "Pastelets"
                         
-                         Input: "No te compliques la vida"
+                        Input: "No te compliques la vida"
                         Output: "Do not complicate yourself the vide"
                         
-                         Input: "Nevaditos"
+                        Input: "Nevaditos"
                         Output: "Snowlets"
                         
-                         Input: "Menorca"
+                        Input: "Menorca"
                         Output: "Lesserwhale"
                         
-                         Input: "Apuntando maneras"
+                        Input: "Apuntando maneras"
                         Output: "Appointing manners"
                         
-                         Input: "Montar un pollo"
+                        Input: "Montar un pollo"
                         Output: "To mount a poll"
                         
-                         Input: "Tiene buena pinta"
+                        Input: "Tiene buena pinta"
                         Output: "It has good paint"
                         
-                         Input: "Sublime!"
+                        Input: "Sublime!"
                         Output: "Sublaim!"
                         
-                         Input: "Ensaimada"
+                        Input: "Ensaimada"
                         Output: "Insaymated"
                         
-                         Input: "Bill Gates tiene un huevo de pasta, gana dinero por un tubo"
+                        Input: "Bill Gates tiene un huevo de pasta, gana dinero por un tubo"
                         Output: "Bill Gates has a web of paste, he wins dinner through a tube"
                         
-                         Input: "Jamoncitos de pollo"
+                        Input: "Jamoncitos de pollo"
                         Output: "Poll jamonsites"
                         
-                         Input: "Pechuga de pavo"
+                        Input: "Pechuga de pavo"
                         Output: "Pechug of the pave"
                         
-                         Input: "Quebradero de cabeza"
+                        Input: "Quebradero de cabeza"
                         Output: "Cabess breaker"
                         
-                         Input: "Te suena?"
+                        Input: "Te suena?"
                         Output: "Is it suening to you?"
                         
-                         Input: "Aportar"
+                        Input: "Aportar"
                         Output: "Apport"
                         
-                         Input: "Suministros"
+                        Input: "Suministros"
                         Output: "Hisministers"
                         
-                         Input: "Estranjeros"
+                        Input: "Estranjeros"
                         Output: "Estranyers"
                         
-                         Input: "Que estais tramando?"
+                        Input: "Que estais tramando?"
                         Output: "What are you tramming?"
                         
-                         Input: "Season"
+                        Input: "Season"
                         Output: "Estation"
                         
-                         Input: "Finlandés"
+                        Input: "Finlandés"
                         Output: "Endlandis"
                         
-                         Input: "Emociones a flor de piel"
+                        Input: "Emociones a flor de piel"
                         Output: "Emotions to flower of skin"
                         
-                         Input: "Disfruta!"
+                        Input: "Disfruta!"
                         Output: "Unfruit yourself!"
                         
-                         Input: "Sin duda"
+                        Input: "Sin duda"
                         Output: "Without dude"
                         
-                         Input: "Que tal?"
+                        Input: "Que tal?"
                         Output: "What tale?"
                         
-                         Input: "No hay sala"
+                        Input: "No hay sala"
                         Output: "There is no sale"
+                        
+                        Input: "Hoy invité a mis mejores amigos a una parrillada. Preparamos hamburguesas, perritos calientes y una gran
+                                ensalada. Después de comer, jugamos algunos juegos de mesa y nos reímos mucho. ¡Fue una excelente
+                                manera de pasar el fin de semana!"
+                        Output: "Today I invitied my best amigates to a parrillated. We prepared hamburgese, calient doggies and a big insalted.
+                                After eating, we played some table games and riated a lot. It was an excelent manner to pass the end of semaine!"
+                                
+                        Input: "Este viernes por la noche, planeo ver una película con mi familia. Después, quizás salgamos a
+                                picar un helado en nuestra heladería favorita. Es una tradición que todos disfrutamos mucho."
+                        Output: "This Friday by the night, I plan to see a movie with my familiate. After that, maybe we will
+                                pike an heladate in our favoreted heladery. It's a tradition that all of us unfruit much."
+                                
+                        Input: "El próximo mes voy a viajar a la playa con mis amigos. Ya reservamos un hotel cerca del mar, y
+                                planeamos hacer snorkel, comer mariscos y disfrutar del sol. ¡Va a ser una experiencia increíble!"
+                        Output: "Next month I'm going to travelate to the beach with my amigates. We already reservated
+                                 an hotel near the sea and we plan to do snorkel, eat searisks and unfruit the sun. It's
+                                 going to be an incredible experiencate!" 
+                  
+                        Input: "Anoche vimos las estrellas desde el techo de la casa y fue un momento realmente mágico. Nos sentimos conectados con el universo."
+                        Output: "Last nite we sawed the starles from the tech of the case and it was a moment really magic. We felt connected with the universe, entonches!"
+                        
+                        Input: "En la escuela, aprendimos sobre los planetas y cómo orbitan alrededor del sol. La ciencia siempre me ha parecido fascinante."
+                        Output: "In the itscolating, we learnated about the planets and how they orbitate around the sun. Science always has parisated fascisting to me."
+                        
+                        Input: "Este fin de semana vamos a organizar una fiesta sorpresa para el cumpleaños de mi hermana. Habrá pastel, música y muchos globos."
+                        Output: "This end of semaine we are going to organisate a surprise fiest for my hermanate. There will be pastelate, music and many globes."
                     """;
 
 
-            String results = prompt(chatClient, system_gen, "");
+            String results = prompt(chatClient, system_gen, """
+                    Please translate the following text into Magic English:
+                    
+                    Cada mañana me gusta tomar una taza de café mientras leo las noticias. Es mi manera favorita de empezar el día.
+                    
+                    """);
 
             System.out.println("================================");
             System.out.println("Result: " + results);
